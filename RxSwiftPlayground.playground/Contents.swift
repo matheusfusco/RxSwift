53d9becad1374bc72a2e5d260117648fb84a06db
Matheus Pacheco <matheuspacheco@MacBook-Pro-Matheus.local>
2018-08-10 14:02:03 -0300

    Estudando novas features no Rx.

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -36,29 +36,13 @@ example(of: "empty") {
 
 example(of: "never") {
   let observable = Observable<Any>.never()
-  let disposeBag = DisposeBag()
-//  observable
-//    .subscribe(onNext: { element in
-//      print(element)
-//    },
-//     onCompleted: {
-//      print("Completed")
-//    })
-  
   observable
-  .do(onNext: { element in
-        print(element)
-  },  onError: { error in
-        print(error)
-  }, onCompleted: {
-        print("Completed")
-  }, onSubscribe: {
-        print("On Subscribe")
-  }, onSubscribed: {
-        print("Subscribed")
-  }, onDispose: {
-        print("Disposed")
-  })
+    .subscribe(onNext: { element in
+      print(element)
+    },
+     onCompleted: {
+      print("Completed")
+    })
 }
 
 
@@ -147,24 +131,88 @@ example(of: "Single") {
   }
     .disposed(by: disposeBag)
 }
-/*:
- Copyright (c) 2014-2018 Razeware LLC
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+
+
+
+example(of: "Never - Do") {
+  let observable = Observable<Any>.never()
+  let disposeBag = DisposeBag()
+  
+  observable
+    .do(onNext: { element in
+      print(element)
+    },  onError: { error in
+      print(error)
+    }, onCompleted: {
+      print("Do: Completed")
+    }, onSubscribe: {
+      print("Do:  Subscribe")
+    }, onSubscribed: {
+      print("Do: Subscribed")
+    }, onDispose: {
+      print("Do: Dispose")
+    })
+    .subscribe(onNext: { element in
+      print(element)
+    }, onError: { error in
+      print(error)
+    }, onCompleted: {
+      print("Completed")
+    }, onDisposed: {
+      print("Disposed")
+    })
+    .disposed(by: disposeBag)
+}
+
+example(of: "PublishSubject") {
+  let quotes = PublishSubject<String>()
+  quotes.onNext(itsNotMyFault)
+  
+  let subscriptionOne = quotes
+    .subscribe {
+      print(label: "1)", event: $0)
+  }
+  quotes.on(.next(doOrDoNot))
+  
+  let subscriptionTwo = quotes
+    .subscribe {
+      print(label: "2)", event: $0)
+  }
+  
+  quotes.onNext(lackOfFaith)
+  
+  subscriptionOne.dispose()
+  
+  quotes.onNext(eyesCanDeceive)
+  
+  quotes.onCompleted()
+  
+  let subscriptionThree = quotes
+    .subscribe {
+      print("3)", $0)
+  }
+  
+  quotes.onNext(stayOnTarget)
+  
+  subscriptionOne.dispose()
+  subscriptionTwo.dispose()
+  subscriptionThree.dispose()
+  
+}
+
+example(of: "BehaviorSubject") {
+  let disposeBag = DisposeBag()
+  
+  let quotes = BehaviorSubject(value: iAmYourFather)
+  
+  let subscriptionOne = quotes
+    .subscribe {
+      print(label: "1)", event: $0)
+  }
+  quotes.onError(Quote.neverSaidThat)
+  
+  let subscriptionTwo = quotes
+    .subscribe {
+      print(label: "2)", event: $0)
+  }
+}
