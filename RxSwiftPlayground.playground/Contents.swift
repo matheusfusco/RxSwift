40ac98ef0ad7a448799f24b3109325ed687f39b8
Matheus Fusco <matheusfusco@gmail.com>
2018-09-04 14:55:16 -0300

    IgnoreElements, ElementAt, Filter, SkipWhile, SkipUntil, DistinctUntilChanged, Map, FlatMap, FlatMapLatest, StartWith, Concat, Merge, CombineLatest, Zip, Amb, Reduce, Scan

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -11,10 +11,10 @@ example(of: "creating observables") {
 
 example(of: "subscribe") {
   let observable = Observable.of(episodeIV, episodeV, episodeVI)
-//  observable.subscribe { event in
-////    print(event)
-//    print(event.element ?? event)
-//  }
+  //  observable.subscribe { event in
+  ////    print(event)
+  //    print(event.element ?? event)
+  //  }
   
   observable.subscribe(onNext: { element in
     print(element)
@@ -24,13 +24,13 @@ example(of: "subscribe") {
 
 example(of: "empty") {
   let observable = Observable<Void>.empty()
-
+  
   observable
     .subscribe(onNext: { element in
       print(element)
     },
-     onCompleted: {
-      print("Completed")
+               onCompleted: {
+                print("Completed")
     })
 }
 
@@ -40,8 +40,8 @@ example(of: "never") {
     .subscribe(onNext: { element in
       print(element)
     },
-     onCompleted: {
-      print("Completed")
+               onCompleted: {
+                print("Completed")
     })
 }
 
@@ -59,10 +59,10 @@ example(of: "dispose") {
 example(of: "DisposeBag") {
   let disposeBag = DisposeBag()
   Observable.of(episodeVII, episodeI, rogueOne)
-  .subscribe({
-    print($0)
-  })
-  .disposed(by: disposeBag)
+    .subscribe({
+      print($0)
+    })
+    .disposed(by: disposeBag)
 }
 
 example(of: "create") {
@@ -74,17 +74,17 @@ example(of: "create") {
   Observable<String>.create { observer in
     
     observer.onNext("R2-D2")
-//    observer.onError(Droid.OU812)
+    //    observer.onError(Droid.OU812)
     observer.onNext("C-3PO")
     observer.onNext("K-2SO")
     observer.onCompleted()
     return Disposables.create()
-  }
-  .subscribe(onNext: { print($0) },
-             onError: { print("Error: ", $0) },
-             onCompleted: { print("Completed") },
-             onDisposed: { print("Disposed") }
-  ).disposed(by: disposeBag)
+    }
+    .subscribe(onNext: { print($0) },
+               onError: { print("Error: ", $0) },
+               onCompleted: { print("Completed") },
+               onDisposed: { print("Disposed") }
+    ).disposed(by: disposeBag)
 }
 
 
@@ -128,7 +128,7 @@ example(of: "Single") {
       case .error (let error):
         print(error)
       }
-  }
+    }
     .disposed(by: disposeBag)
 }
 
@@ -214,7 +214,7 @@ example(of: "BehaviorSubject") {
   let subscriptionTwo = quotes
     .subscribe {
       print(label: "2)", event: $0)
-  }
+    }
     .disposed(by: disposeBag)
 }
 
@@ -228,16 +228,16 @@ example(of: "ReplaySubject") {
   subject
     .subscribe {
       print(label: "1)", event: $0)
-  }
-  .disposed(by: disposeBag)
+    }
+    .disposed(by: disposeBag)
   
   subject.onNext(theForceIsStrong)
-
+  
   subject
     .subscribe {
       print(label: "2)", event: $0)
-  }
-  .disposed(by: disposeBag)
+    }
+    .disposed(by: disposeBag)
 }
 
 
@@ -251,13 +251,325 @@ example(of: "Variable") {
   variable.asObservable()
     .subscribe {
       print(label: "1)", event: $0)
-  }
-  .disposed(by: disposeBag)
+    }
+    .disposed(by: disposeBag)
   
   variable.value = mayThe4thBeWithYou
-
+  
   //não é possivel - erros não podem ser atribuidos a variables
-//  variable.value = MyError.anError
-//  variable.asObservable().onError(MyError.anError)
-//  variable.asObservable().onCompleted()
+  //  variable.value = MyError.anError
+  //  variable.asObservable().onError(MyError.anError)
+  //  variable.asObservable().onCompleted()
+}
+
+example(of: "ignoreElements") {
+  let disposeBag = DisposeBag()
+  
+  let cannedProjects = PublishSubject<String>()
+  
+  cannedProjects
+    .ignoreElements()
+    .subscribe {
+      print($0)
+    }
+    .disposed(by: disposeBag)
+  
+  cannedProjects.onNext(landOfDroids)
+  cannedProjects.onNext(wookieWorld)
+  cannedProjects.onNext(detours)
+  
+  cannedProjects.onCompleted()
+}
+
+example(of: "elementAt") {
+  let disposeBag = DisposeBag()
+  
+  let quotes = PublishSubject<String>()
+  
+  quotes
+    .elementAt(2)
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+  
+  quotes.onNext(mayTheOdds)
+  quotes.onNext(liveLongAndProsper)
+  quotes.onNext(mayTheForce)
+}
+
+example(of: "filter") {
+  let disposeBag = DisposeBag()
+  
+  Observable.from(tomatometerRatings)
+    .filter{ movie in
+      movie.rating >= 90
+    }
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+  
+}
+
+example(of: "skipWhile") {
+  let disposeBag = DisposeBag()
+  
+  Observable.from(tomatometerRatings)
+    .skipWhile { movie in
+      movie.rating < 90
+    }
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+}
+
+
+example(of: "skipUntil") {
+  let disposeBag = DisposeBag()
+  
+  let subject = PublishSubject<(title: String, rating: Int)>()
+  let trigger = PublishSubject<Void>()
+  
+  subject
+    .skipUntil(trigger)
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+  
+  subject.onNext(_episodeI)
+  subject.onNext(_episodeII)
+  subject.onNext(_episodeIII)
+  trigger.onNext(())
+  subject.onNext(_episodeIV)
+}
+
+
+example(of: "distinctUntilChanged") {
+  let disposeBag = DisposeBag()
+  
+  Observable<Droid>.of(.R2D2, .C3PO, .C3PO, .R2D2)
+    .distinctUntilChanged()
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+}
+
+
+example(of: "map") {
+  let disposeBag = DisposeBag()
+  Observable.from(episodes)
+    .map {
+      var components = $0.components(separatedBy: " ")
+      let number = NSNumber(value: try! components[1].romanNumeralIntValue())
+      let numberString = String(describing: number)
+      components[1] = numberString
+      
+      return components.joined(separator: " ")
+    }
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+}
+
+example(of: "flatmap") {
+  let disposeBag = DisposeBag()
+  
+  let ryan = Jedi(rank: BehaviorSubject(value: .youngling))
+  let charlotte = Jedi(rank: BehaviorSubject(value: .youngling))
+  
+  let student = PublishSubject<Jedi>()
+  
+  student
+    .flatMap {
+      $0.rank
+    }
+    .subscribe(onNext: {
+      print($0.rawValue)
+    })
+    .disposed(by: disposeBag)
+  
+  student.onNext(ryan)
+  ryan.rank.onNext(.padawan)
+  student.onNext(charlotte)
+  ryan.rank.onNext(.jediKnight)
+  charlotte.rank.onNext(.jediMaster)
+}
+
+example(of: "flatmap latest") {
+  let disposeBag = DisposeBag()
+  
+  let ryan = Jedi(rank: BehaviorSubject(value: .youngling))
+  let charlotte = Jedi(rank: BehaviorSubject(value: .youngling))
+  
+  let student = PublishSubject<Jedi>()
+  
+  student
+    .flatMapLatest {
+      $0.rank
+    }
+    .subscribe(onNext: {
+      print($0.rawValue)
+    })
+    .disposed(by: disposeBag)
+  
+  student.onNext(ryan)
+  ryan.rank.onNext(.padawan)
+  student.onNext(charlotte)
+  ryan.rank.onNext(.jediKnight)
+  charlotte.rank.onNext(.jediMaster)
+}
+
+
+example(of: "startWith") {
+  let disposeBag = DisposeBag()
+  let prequelEpisodes = Observable.of(episodeI, episodeII, episodeIII)
+  let flashback = prequelEpisodes.startWith(episodeIV, episodeV)
+  
+  flashback.subscribe(onNext: { episode in
+    print(episode)
+  })
+    .disposed(by: disposeBag)
+}
+
+example(of: "concat") {
+  let disposeBag = DisposeBag()
+  
+  let prequelTrilogy = Observable.of(episodeI, episodeII, episodeIII)
+  let originalTrilogy = Observable.of(episodeIV, episodeV, episodeVI)
+  
+  prequelTrilogy.concat(originalTrilogy)
+    .subscribe(onNext: { episode in
+      print(episode)
+    })
+    .disposed(by: disposeBag)
+}
+
+example(of: "merge") {
+  let disposeBag = DisposeBag()
+  
+  let filmTrilogies = PublishSubject<String>()
+  let standAloneFilms = PublishSubject<String>()
+  let storyOrder = Observable.of(filmTrilogies, standAloneFilms)
+  
+  storyOrder.asObservable()
+    .merge()
+    .subscribe(onNext: { film in
+      print(film)
+    })
+    .disposed(by: disposeBag)
+  
+  filmTrilogies.onNext(__episodeI)
+  filmTrilogies.onNext(__episodeII)
+  
+  standAloneFilms.onNext(theCloneWars)
+  
+  filmTrilogies.onNext(__episodeIII)
+  standAloneFilms.onNext(solo)
+  standAloneFilms.onNext(rogueOne)
+  
+  filmTrilogies.onNext(__episodeIV)
+  filmTrilogies.onNext(__episodeV)
+  filmTrilogies.onNext(__episodeVI)
+}
+
+
+example(of: "combineLatest") {
+  let disposeBag = DisposeBag()
+  
+  let characters = Observable.of(luke, hanSolo, leia, chewbacca)
+  let primaryWeapons = Observable.of(lightsaber, dl44, defender, bowcaster)
+  
+  Observable.combineLatest(characters, primaryWeapons) { character, weapon in
+    "\(character): \(weapon)"
+    }
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+}
+
+
+example(of: "zip") {
+  let disposeBag = DisposeBag()
+  
+  let characters = Observable.of(luke, hanSolo, leia, chewbacca)
+  let primaryWeapons = Observable.of(lightsaber, dl44, defender, bowcaster)
+  
+  Observable.zip(characters, primaryWeapons) { character, weapon in
+    "\(character): \(weapon)"
+    }
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+}
+
+
+example(of: "amb") {
+  let disposeBag = DisposeBag()
+  
+  let prequelEpisodes = PublishSubject<String>()
+  let originalEpisodes = PublishSubject<String>()
+  
+  prequelEpisodes.amb(originalEpisodes)
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+  
+  originalEpisodes.onNext(__episodeIV)
+  
+  prequelEpisodes.onNext(__episodeI)
+  prequelEpisodes.onNext(__episodeII)
+  
+  originalEpisodes.onNext(__episodeV)
+}
+
+
+example(of: "reduce") {
+  let disposeBag = DisposeBag()
+  
+  Observable.from(runtimes.values)
+    .reduce(0, accumulator: +)
+    .subscribe(onNext: {
+      print(stringFrom($0))
+    })
+    .disposed(by: disposeBag)
+}
+
+
+example(of: "scan") {
+  let disposeBag = DisposeBag()
+  
+  Observable.from(runtimes.values)
+    .scan(0, accumulator: +)
+    .subscribe(onNext: {
+      print(stringFrom($0))
+    })
+    .disposed(by: disposeBag)
+}
+
+
+example(of: "challenge: zip + scan") {
+  let disposeBag = DisposeBag()
+  
+  let runTimeKeyValues = Observable.from(runtimes)
+  
+  let scanTotal = runTimeKeyValues.scan(0) { total, movie in
+    total + movie.value
+  }
+  
+  let results = Observable.zip(runTimeKeyValues, scanTotal) {
+    ($0.key, $0.value, $1)
+  }
+  
+  results.subscribe(onNext: {
+    print("Filme: \($0) - Duração do filme: \(stringFrom($1)) - Tempo total da maratona: \(stringFrom($2))")
+  })
+    .disposed(by: disposeBag)
 }
--- RxSwiftPlayground.playground/Sources/SupportCode.swift
+++ RxSwiftPlayground.playground/Sources/SupportCode.swift
@@ -42,3 +42,133 @@ public let useTheForce = "Use the Force, Luke."
 public let theForceIsStrong = "The Force is strong with this one."
 public let mayTheForceBeWithYou = "May the Force be with you."
 public let mayThe4thBeWithYou = "May the 4th be with you."
+
+
+
+public let landOfDroids = "Land of Droids"
+public let wookieWorld = "Wookie World"
+public let detours = "Detours"
+
+
+public let mayTheOdds = "And may the odds be ever in your favor"
+public let liveLongAndProsper = "Live long and prosper"
+public let mayTheForce = "May the Force be with you"
+
+
+public let _episodeI = (title: "The Phantom Menace", rating: 55)
+public let _episodeII = (title: "Attack of the Clones", rating: 66)
+public let _episodeIII = (title: "Revenge of the Sith", rating: 79)
+public let _rogueOne = (title: "Rogue One", rating: 85)
+public let _episodeIV = (title: "A New Hope", rating: 93)
+public let _episodeV = (title: "The Empire Strikes Back", rating: 94)
+public let _episodeVI = (title: "Return Of The Jedi", rating: 80)
+public let _episodeVII = (title: "The Force Awakens", rating: 93)
+public let _episodeVIII = (title: "The Last Jedi", rating: 91)
+public let tomatometerRatings = [_episodeI, _episodeII, _episodeIII, _rogueOne, _episodeIV, _episodeV, _episodeVI, _episodeVII, _episodeVIII]
+
+public enum Droid {
+    case C3PO, R2D2
+}
+
+
+public let __episodeI = "Episode I - The Phantom Menace"
+public let __episodeII = "Episode II - Attack of the Clones"
+public let __episodeIII = "Episode III - Revenge of the Sith"
+public let __episodeIV = "Episode IV - A New Hope"
+public let __episodeV = "Episode V - The Empire Strikes Back"
+public let __episodeVI = "Episode VI - Return Of The Jedi"
+public let __episodeVII = "Episode VII - The Force Awakens"
+public let __episodeVIII = "Episode VIII - The Last Jedi"
+public let __episodeIX = "Episode IX"
+
+public let episodes = [__episodeI, __episodeII, __episodeIII, __episodeIV, __episodeV, __episodeVI, __episodeVII, __episodeVIII, __episodeIX]
+
+public extension String {
+    
+    /// https://stackoverflow.com/a/36949832/616764
+    func romanNumeralIntValue() throws -> Int  {
+        if range(of: "^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$", options: .regularExpression) == nil  {
+            throw NSError(domain: "NotValidRomanNumber", code: -1, userInfo: nil)
+        }
+        
+        var result = 0
+        var maxValue = 0
+        
+        uppercased().reversed().forEach {
+            let value: Int
+            switch $0 {
+            case "M":
+                value = 1000
+            case "D":
+                value = 500
+            case "C":
+                value = 100
+            case "L":
+                value = 50
+            case "X":
+                value = 10
+            case "V":
+                value = 5
+            case "I":
+                value = 1
+            default:
+                value = 0
+            }
+            
+            maxValue = max(value, maxValue)
+            result += value == maxValue ? value : -value
+        }
+        
+        return result
+    }
+}
+
+public struct Jedi {
+    
+    public var rank: BehaviorSubject<JediRank>
+    
+    public init(rank: BehaviorSubject<JediRank>) {
+        self.rank = rank
+    }
+}
+
+public enum JediRank: String {
+    
+    case youngling = "Youngling"
+    case padawan = "Padawan"
+    case jediKnight = "Jedi Knight"
+    case jediMaster = "Jedi Master"
+    case jediGrandMaster = "Jedi Grand Master"
+}
+
+
+public let luke = "Luke Skywalker"
+public let hanSolo = "Han Solo"
+public let leia = "Princess Leia"
+public let chewbacca = "Chewbacca"
+
+public let lightsaber = "Lightsaber"
+public let dl44 = "DL-44 Blaster"
+public let defender = "Defender Sporting Blaster"
+public let bowcaster = "Bowcaster"
+
+let formatter = DateComponentsFormatter()
+
+public func stringFrom(_ minutes: Int) -> String {
+    let interval = TimeInterval(minutes)
+    return formatter.string(from: interval) ?? ""
+}
+
+public let runtimes = [
+    __episodeI: 136,
+    __episodeII: 142,
+    theCloneWars: 98,
+    __episodeIII: 140,
+    solo: 142,
+    rogueOne: 142,
+    __episodeIV: 121,
+    __episodeV: 124,
+    __episodeVI: 134,
+    __episodeVII: 136,
+    __episodeVIII: 152
+]
